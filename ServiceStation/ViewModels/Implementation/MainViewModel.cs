c4682d5ffeae4ded47317c394d132ab158ed36a3
FoxInGloves <dparhomenko84@gmail.com>
2024-10-28 22:55:37 +0300

    feat!: change main view this change is temporarily

--- ServiceStation/ViewModels/Implementation/MainViewModel.cs
+++ ServiceStation/ViewModels/Implementation/MainViewModel.cs
@@ -1,8 +1,46 @@
-﻿using ServiceStation.ViewModels.Abstraction;
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using ServiceStation.Services.Command;
+using ServiceStation.ViewModels.Abstraction;
 
 namespace ServiceStation.ViewModels.Implementation;
 
 public class MainViewModel : BaseViewModel
 {
-    public string Message { get; set; }
+    private double _widthOfNavigationPanel;
+    
+    private bool _isExpanded = false;
+    private const double CollapsedWidth = 50; // Минимальная ширина
+    private const double ExpandedWidth = 300;
+    
+    public MainViewModel()
+    {
+        MoveNavigationPanelCommand = new RelayCommand(_ => ToggleSize(), _ => true);
+       _widthOfNavigationPanel = CollapsedWidth;
+    }
+    
+    public ICommand MoveNavigationPanelCommand { get; init; }
+    
+    public double WidthOfNavigationPanel 
+    {
+        get => _widthOfNavigationPanel;
+        
+        set => SetField(ref _widthOfNavigationPanel, value);
+    }
+    
+    private void ToggleSize()
+    {
+        if (_isExpanded)
+        {
+            WidthOfNavigationPanel = CollapsedWidth;
+            _isExpanded = !_isExpanded;
+        }
+        else
+        {
+            WidthOfNavigationPanel = ExpandedWidth;
+            _isExpanded = !_isExpanded;
+        }
+        
+    }
 }
