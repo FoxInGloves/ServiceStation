b93204d0e37a8a103d1a950de5e3a74b33c5b816
FoxInGloves <dparhomenko84@gmail.com>
2024-10-30 19:49:59 +0300

    feat(view): add colorfull text template

---
+++ ServiceStation/Views/UserControls/ColorfulText.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ServiceStation.Views.UserControls;
+
+public partial class ColorfulText : UserControl
+{
+    public ColorfulText()
+    {
+        InitializeComponent();
+    }
+}
