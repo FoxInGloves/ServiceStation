8ff5ddb30468289155ed8c4f56b25c7ee25ff0dc
FoxInGloves <dparhomenko84@gmail.com>
2024-10-30 20:37:51 +0300

    feat(view): add own properties for colorful text

--- ServiceStation/Views/UserControls/ColorfulText.xaml.cs
+++ ServiceStation/Views/UserControls/ColorfulText.xaml.cs
@@ -1,11 +1,35 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ServiceStation.Views.UserControls;
 
 public partial class ColorfulText : UserControl
 {
+    //TODO Обратить внимание на defaultValue в Metadata. Не уверен что хначение будет работать
+    private new static readonly DependencyProperty BackgroundProperty =
+        DependencyProperty.Register(nameof(BackgroundColor), typeof(string), typeof(ColorfulText),
+            new FrameworkPropertyMetadata("FFFFFF", FrameworkPropertyMetadataOptions.None));
+    
+    private new static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(nameof(Text), typeof(string), typeof(ColorfulText),
+            new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.None));
+    
     public ColorfulText()
     {
         InitializeComponent();
+    }
+
+    public string BackgroundColor
+    {
+        get => (string)GetValue(BackgroundProperty);
+        
+        set => SetValue(BackgroundProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        
+        set => SetValue(TextProperty, value);
     }
 }
