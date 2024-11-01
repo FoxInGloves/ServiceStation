14a19f37d69e55f2a0068cf85777437ea1326e42
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-11-01 21:01:07 +0300

    feat(mapper): add IMapper interface and half realized VehicleMapper

---
+++ ServiceStation/Services/Mapping/Abstraction/IMapper.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStation.Models.DTOs.Abstraction;
+using ServiceStation.Models.Entities.Abstraction;
+
+namespace ServiceStation.Services.Mapping.Abstraction;
+
+//TODO сделать lheujq маппер
+public interface IMapper<TSource, TDestination> where TSource : AbstractEntity where TDestination : AbstractDto
+{
+    TDestination MapToDto(TSource source);
+    
+    TSource MapToEntity(TDestination destination);
+}
---
+++ ServiceStation/Services/Mapping/Implementation/VehicleMapper.cs
@@ -0,0 +1,26 @@
+﻿using ServiceStation.Models.DTOs.Implementation;
+using ServiceStation.Models.Entities.Implementation;
+using ServiceStation.Services.Mapping.Abstraction;
+
+namespace ServiceStation.Services.Mapping.Implementation;
+
+public class VehicleMapper : IMapper<Vehicle, VehicleDto>
+{
+    public VehicleDto MapToDto(Vehicle source)
+    {
+        return new VehicleDto
+        {
+            Id = source.Id,
+            Brand = source.BrandOfVehicle.Brand,
+            Model = source.BrandOfVehicle.Model,
+            LicenseNumber = source.LicenseNumber,
+            Status = source.Status,
+            YearOfRelease = source.YearOfRelease
+        };
+    }
+
+    public Vehicle MapToEntity(VehicleDto destination)
+    {
+        throw new NotImplementedException();
+    }
+}
