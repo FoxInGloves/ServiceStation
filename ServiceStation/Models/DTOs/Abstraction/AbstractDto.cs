f61e5e9ae61fecd400dbbcb7d22a079acab8ecb5
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-11-01 20:59:40 +0300

    feat(dto): add dto directory, abstractDto and VehicleDto

---
+++ ServiceStation/Models/DTOs/Abstraction/AbstractDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ServiceStation.Models.DTOs.Abstraction;
+
+public abstract class AbstractDto
+{
+    public Guid Id { get; set; }
+}
---
+++ ServiceStation/Models/DTOs/Implementation/VehicleDto.cs
@@ -0,0 +1,18 @@
+﻿using ServiceStation.Models.DTOs.Abstraction;
+
+namespace ServiceStation.Models.DTOs.Implementation;
+
+public class VehicleDto : AbstractDto
+{
+    public string Brand { get; set; }
+    
+    public string Model { get; set; }
+    
+    public DateOnly YearOfRelease { get; set; }
+    
+    public string LicenseNumber { get; set; }
+    
+    public string Status { get; set; }
+    
+    public string StatusColor { get; set; }
+}
