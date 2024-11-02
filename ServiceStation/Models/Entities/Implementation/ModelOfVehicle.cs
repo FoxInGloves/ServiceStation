854f90727f28dfb912b3b2ac483db7cb2777f752
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-11-02 18:35:51 +0300

    feat(entity): add new model entity

---
+++ ServiceStation/Models/Entities/Implementation/ModelOfVehicle.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStation.Models.Entities.Abstraction;
+
+namespace ServiceStation.Models.Entities.Implementation;
+
+public class ModelOfVehicle : AbstractEntity
+{
+    public Guid BrandId { get; set; }
+    
+    public string Name { get; set; }
+    
+    public virtual BrandOfVehicle Brand { get; set; }
+}
