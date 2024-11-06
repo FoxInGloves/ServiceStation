cb1460cb34f6300c14cb9014d8c50da851de4ba4
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-11-06 18:02:00 +0300

    feat: add navigation to vehicle page

--- ServiceStation/ViewModels/Implementation/MainViewModel.cs
+++ ServiceStation/ViewModels/Implementation/MainViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
-using ServiceStation.Services.Command;
 using ServiceStation.Services.Navigation.Abstraction;
 using ServiceStation.ViewModels.Abstraction;
+using RelayCommand = ServiceStation.Services.Command.RelayCommand;
 
 namespace ServiceStation.ViewModels.Implementation;
 
@@ -28,11 +29,14 @@ public sealed class MainViewModel : AbstractViewModel
         
         ToggleNavigationPanelCommand = new RelayCommand(_ => ToggleNavigationPanelWidth(), _ => true);
         _navigationColumnWidth = NormalNavigationPanelWidth;
+        NavigateToPageCommandAsync = new AsyncRelayCommand(_ => NavigateToVehiclesPageAsync());
        
        _logger.LogInformation("MainViewModel initialized");
     }
     
     public ICommand ToggleNavigationPanelCommand { get; init; }
+    
+    public ICommand NavigateToPageCommandAsync { get; init; }
 
     public int NavigationColumnWidth
     {
@@ -50,9 +54,41 @@ public sealed class MainViewModel : AbstractViewModel
 
     private void ToggleNavigationPanelWidth()
     {
-        NavigationColumnWidth = _navigationColumnWidth == ExpandedNavigationPanelWidth ? 
+        NavigationColumnWidth = _navigationColumnWidth == ExpandedNavigationPanelWidth ?
             NormalNavigationPanelWidth : ExpandedNavigationPanelWidth;
-        
-        _logger.LogInformation("NavigationColumnWidth toggled");
+
+    }
+
+    private async Task SetCurrentPage((Page, AbstractViewModel) pageAndViewModel)
+    {
+        var updateVm = UpdateViewModel(pageAndViewModel.Item2);
+        CurrentPage = pageAndViewModel.Item1;
+        await updateVm;
+    }
+
+    private static async Task UpdateViewModel(AbstractViewModel viewModel)
+    {
+        await viewModel.UpdateAsync();
+    }
+
+    private async Task NavigateToVehiclesPageAsync()
+    {
+        if (_vehiclesPageAndViewModel == null)
+        {
+
+            var navigationResult = _navigationService.NavigateToPage<VehiclesViewModel>();
+
+            if (!navigationResult.IsSuccess)
+            {
+                _logger.LogError("Failed navigated to vehicles page.\nCode: {Code}\nDescription: {Description}",
+                    navigationResult.Error?.Code, navigationResult.Error?.Description);
+                return;
+            }
+
+            var vehicleViewAndViewModel = navigationResult.Value;
+            _vehiclesPageAndViewModel = vehicleViewAndViewModel;
+        }
+
+        await SetCurrentPage(_vehiclesPageAndViewModel.Value);
     }
 }
