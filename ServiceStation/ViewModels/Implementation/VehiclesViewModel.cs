da09ffcd646247e2637f6b136f0689e70de12318
Dmitriy Parhomenko <dparhomenko84@gmail.com>
2024-11-29 08:50:59 +0300

    refactor: rename vehicle mapper and improve vehicle deletion

--- ServiceStation/ViewModels/Implementation/VehiclesViewModel.cs
+++ ServiceStation/ViewModels/Implementation/VehiclesViewModel.cs
@@ -19,7 +19,7 @@ public sealed class VehiclesViewModel : AbstractViewModel
 {
     private readonly ILogger<VehiclesViewModel> _logger;
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IMapper<Vehicle, VehicleDto> _mapper;
+    private readonly IMapper<Vehicle, VehicleDto> _vehicleMapper;
     private readonly INavigationService _navigationService;
 
     private ObservableCollection<VehicleDto>? _collectionOfVehicles;
@@ -30,7 +30,7 @@ public sealed class VehiclesViewModel : AbstractViewModel
     {
         _logger = logger;
         _unitOfWork = unitOfWork;
-        _mapper = mapper;
+        _vehicleMapper = mapper;
         _navigationService = navigationService;
 
         AddNewVehicleAsyncCommand = new AsyncRelayCommand(AddNewVehicle);
@@ -64,13 +64,14 @@ public sealed class VehiclesViewModel : AbstractViewModel
         _logger.LogInformation("Updating vehicles");
         //TODO разобраться нужно ли создавать каждый раз новую коллекцию
         var vehicles = await _unitOfWork.VehicleRepository.GetAsync();
-        var vehicleDto = _mapper.MapToDtos(vehicles);
+        var vehicleDto = _vehicleMapper.MapToDtos(vehicles);
 
         CollectionOfVehicles = new ObservableCollection<VehicleDto>(vehicleDto);
         string a;
         string b;
     }
 
+    //TODO рефактор метода
     private async Task AddNewVehicle()
     {
         var newVehicleResult = OpenAddNewVehicleWindow();
@@ -88,56 +89,54 @@ public sealed class VehiclesViewModel : AbstractViewModel
         if (owner is null) throw new ArgumentNullException(nameof(owner));
         var addOwnerTask = _unitOfWork.OwnersRepository.CreateAsync(owner);
 
-        var model = vehicle.ModelOfVehicle;
-        if (model is null)
-            throw new NullReferenceException(nameof(model));
-        var brand = model.Brand;
+        var modelOfVehicle = vehicle.ModelOfVehicle;
+        if (modelOfVehicle is null)
+            throw new NullReferenceException(nameof(modelOfVehicle));
+        
+        var brand = modelOfVehicle.Brand;
         if (brand is null)
             throw new NullReferenceException(nameof(brand));
 
         var brandsOnDb = await _unitOfWork.BrandsOfVehicleRepository.GetAsync();
-        var dbBrand =
+        var existingBrand =
             brandsOnDb.FirstOrDefault(x => x.Name.Equals(brand.Name, StringComparison.CurrentCultureIgnoreCase));
 
-        Task? addBrandTask;
-        if (dbBrand == null)
+        Task addBrandTask;
+        if (existingBrand == null)
         {
             addBrandTask = _unitOfWork.BrandsOfVehicleRepository.CreateAsync(brand);
         }
         else
         {
             addBrandTask = Task.CompletedTask;
-            model.BrandId = brand.Id;
-            model.Brand = brand;
+            modelOfVehicle.BrandId = existingBrand.Id;
+            modelOfVehicle.Brand = existingBrand;
         }
 
         var modelsOnDb = await _unitOfWork.ModelsRepository.GetAsync();
-        var dbModel = modelsOnDb.FirstOrDefault(x =>
-            x.Name.Equals(model.Name, StringComparison.CurrentCultureIgnoreCase) &&
-            x.BrandId == model.BrandId);
-        Task? addModelTask;
-        if (dbModel == null)
+        var existingModel = modelsOnDb.FirstOrDefault(x =>
+            x.Name.Equals(modelOfVehicle.Name, StringComparison.CurrentCultureIgnoreCase) &&
+            x.BrandId == modelOfVehicle.BrandId);
+        Task addModelTask;
+        if (existingModel == null)
         {
-            addModelTask = _unitOfWork.ModelsRepository.CreateAsync(model);
+            addModelTask = _unitOfWork.ModelsRepository.CreateAsync(modelOfVehicle);
         }
         else
         {
             addModelTask = Task.CompletedTask;
-            vehicle.ModelOfVehicleId = dbModel.Id;
-            vehicle.ModelOfVehicle = dbModel;
+            vehicle.ModelOfVehicleId = existingModel.Id;
+            vehicle.ModelOfVehicle = existingModel;
         }
 
         var addVehicleTask = _unitOfWork.VehicleRepository.CreateAsync(vehicle);
-        var saveChangesTask = _unitOfWork.SaveChangesAsync();
 
-        var vehicleDto = _mapper.MapToDto(vehicle);
+        var vehicleDto = _vehicleMapper.MapToDto(vehicle);
         CollectionOfVehicles!.Add(vehicleDto);
 
-        await addOwnerTask;
-        await addModelTask;
-        await addBrandTask;
-        await addVehicleTask;
-        await saveChangesTask;
+        await Task.WhenAll(addOwnerTask, addModelTask,  addBrandTask, addVehicleTask);
+        
+        await _unitOfWork.SaveChangesAsync();
     }
 
     private ResultT<Vehicle> OpenAddNewVehicleWindow()
@@ -149,15 +148,15 @@ public sealed class VehiclesViewModel : AbstractViewModel
             return Error.Failure(newVehicleViewAndViewModelResult.Error?.Code!, "Could not open new vehicle window");
         }
 
-        var newWorkerViewAndViewModel = newVehicleViewAndViewModelResult.Value;
-        newWorkerViewAndViewModel.Item1.Owner = Application.Current.MainWindow;
+        var newVehicleViewAndViewModel = newVehicleViewAndViewModelResult.Value;
+        newVehicleViewAndViewModel.Item1.Owner = Application.Current.MainWindow;
 
-        var dialog = newWorkerViewAndViewModel.Item1.ShowDialog();
+        var dialog = newVehicleViewAndViewModel.Item1.ShowDialog();
 
-        if (dialog is not null)
-            return Error.Failure("WindowClosed", "New vehicle window closed");
+        if (dialog is false)
+            return Error.Information("WindowClosed", "New vehicle window closed");
 
-        if (newWorkerViewAndViewModel.Item2 is not AddNewVehicleViewModel newVehicleViewModel)
+        if (newVehicleViewAndViewModel.Item2 is not AddNewVehicleViewModel newVehicleViewModel)
         {
             return Error.Failure("NullReference", "Could not cast AbstractViewModel to AddNewVehicleViewModel");
         }
@@ -169,17 +168,25 @@ public sealed class VehiclesViewModel : AbstractViewModel
 
     private async Task DeleteVehicleAsync(Guid vehicleId)
     {
-        //TODO Может тут выкидывать исключение или try catch
-        /*if (vehicleId is null)
-            return;*/
-        
         var confirmation = MessageBox.Show("Вы уверены?", "Подтверждение", MessageBoxButton.YesNo);
 
         if (confirmation != MessageBoxResult.Yes) return;
+        
+        var vehicle = await _unitOfWork.VehicleRepository.GetByIdAsync(vehicleId);
+        if (vehicle is null) return;
+        
+        /*var defects = await _unitOfWork.DefectsRepository.GetAsync(x => x.VehicleId.Equals(vehicleId));
+        _unitOfWork.DefectsRepository.DeleteRange(defects); */
+        
+        //var deleteOwnerTask = _unitOfWork.OwnersRepository.DeleteByIdAsync(vehicle.OwnerId);
+        var deleteVehicleTask = _unitOfWork.VehicleRepository.DeleteByIdAsync(vehicleId);
+        CollectionOfVehicles?.Remove(CollectionOfVehicles.First(x => x.Id == vehicleId));
+        
+        //await _unitOfWork.DefectsRepository.DeleteByIdAsync(vehicleId);
 
-        //TODO откоментить удаление ТС
-        /*_logger.LogInformation("delete {defect}", vehicleId);
-        await _unitOfWork.DefectsRepository.DeleteAsync(vehicleId);*/
+        //await deleteOwnerTask;
+        await deleteVehicleTask;
+        await _unitOfWork.SaveChangesAsync();
     }
 
     private async Task NavigateToVehicleDetails()
@@ -189,6 +196,18 @@ public sealed class VehiclesViewModel : AbstractViewModel
         if (!vehicleDetailsResult.IsSuccess)
             _logger.LogError("Failed open vehicle detail window.\nCode: {Code}\nDescription: {Description}",
                 vehicleDetailsResult.Error?.Code, vehicleDetailsResult.Error?.Description);
+        
+        var vehicle = await _unitOfWork.VehicleRepository.GetByIdAsync(SelectedVehicle!.Id);
+        if (vehicle == null) throw new NullReferenceException("Vehicle not found");
+        
+        var vehicleDto = _vehicleMapper.MapToDto(vehicle);
+        
+        var vehicleDtoInCollection = CollectionOfVehicles!.First(x => x.Id == vehicle.Id);
+        var vehicleDtoIndex = CollectionOfVehicles!.IndexOf(vehicleDtoInCollection);
+        
+        //CollectionOfVehicles!.Insert(vehicleDtoIndex, vehicleDto);
+        CollectionOfVehicles![vehicleDtoIndex] = vehicleDto;
+        OnPropertyChanged(nameof(CollectionOfVehicles));
     }
 
     private async Task<ResultT<bool>> OpenVehicleDetailWindow()
@@ -208,12 +227,12 @@ public sealed class VehiclesViewModel : AbstractViewModel
             return Error.Failure("NullReference", "Could not cast AbstractViewModel to VehicleDetailsViewModel");
         }
 
-        await vehicleDetailsViewModel.Update(SelectedVehicle!.Id);
+        await vehicleDetailsViewModel.UpdateAsync(SelectedVehicle!.Id);
 
-        var dialog = vehicleDetailsWindowAndViewModel.Item1.ShowDialog();
-
-        if (dialog is not null)
-            return Error.Failure("WindowClosed", "Vehicle details window closed");
+        vehicleDetailsWindowAndViewModel.Item1.ShowDialog();
+        
+        /*var vehicleToUpdate = 
+        await _unitOfWork.VehicleRepository.UpdateAsync()*/
 
         return ResultT<bool>.Success(true);
     }
